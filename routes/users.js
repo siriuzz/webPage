f68acf537439fef66a99f09728551360d16f99dd
siriuzzz <elianmatosd03@gmail.com>
2021-10-02 00:08:04 -0400

    lograda conexion y creacion mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,86 +1,87 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-const main = require('../server');
-const User = require('../models/userModel');
+const bodyParser = require("body-parser");
+const main = require("../server");
+const User = require("../models/userModel");
 
+router.use(bodyParser.json());
 
-router.use(bodyParser.json())
+router.get("/", (req, res) => {
+  res.render("users.ejs");
+});
 
-router.get('/', (req, res) => {
-    res.render('users.ejs');
-})
+router.post("/", (req, res) => {
+  //send data mongodb
+  // (function () {
+  // const { MongoClient } = require('mongodb');
 
+  //     async function main() {
 
-router.post('/',async (req, res) => {
+  //         const uri = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 
-    //send data mongodb
-    // (function () {
-    // const { MongoClient } = require('mongodb');
+  //         const client = new MongoClient(uri);
 
-    //     async function main() {
+  //         async function createListing(client, newListing) {
+  //             const result = await client.db('webDB').collection('users').insertOne(newListing);
 
-    //         const uri = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+  //             console.log('Listing created');
+  //         }
 
-    //         const client = new MongoClient(uri);
+  //         try {
+  //             await client.connect();
 
-    //         async function createListing(client, newListing) {
-    //             const result = await client.db('webDB').collection('users').insertOne(newListing);
+  //             await createListing(client, {
+  //                 name: req.body.nombre,
+  //                 apellido: req.body.apellido
+  //             });
 
-    //             console.log('Listing created');
-    //         }
+  //         } catch (e) {
+  //             console.error(e);
+  //         } finally {
+  //             await client.close();
+  //         }
+  //     }
+  //     main()
+  // })();
 
-    //         try {
-    //             await client.connect();
-
-    //             await createListing(client, {
-    //                 name: req.body.nombre,
-    //                 apellido: req.body.apellido
-    //             });
-
-    //         } catch (e) {
-    //             console.error(e);
-    //         } finally {
-    //             await client.close();
-    //         }
-    //     }
-    //     main()
-    // })();
-
-    //conectar con base de datos mongoose
-    const mongoose = require('mongoose');
-
-    const query = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
-    const db = (query);
-
-    mongoose.Promise = global.Promise
-
-    mongoose.connect(query, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+  //conectar con base de datos mongoose
+  const mongoose = require("mongoose");
+  mongoose.Promise = global.Promise;
+  
+  mongoose.connect(
+      "mongodb://localhost/users",
+      { useNewUrlParser: true },
+      function (error) {
+          if (error) {
+              console.log("Error!" + error);
+            }
+        }
+        );
         
-    }, function (error) {
-        if (error) {
-            console.log("Error!" + error);
-        }
+        //send data mongoose
+        mongoose.connection
+        .once("open", () => {
+            console.log("Connected");
+            
+            const newUser = new User({
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+            });
+            
+            newUser.save((err, data) => {
+                if (err) {
+                    console.log(error);
+                } else {
+                    setTimeout(() => {
+                        res.render('users'); 
+                    }, 3000);
+                }
+      });
     })
+    .on("error", (error) => {
+      console.log("Error:", error);
+    });
 
-
-    //send data mongoose
-    const newUser = new User({
-        nombre: 'yon',
-        apellido: 'klk'
-    })
-
-    await newUser.save((err, data) => {
-        if (err) {
-            console.log(error);
-        } else {
-            res.send('Data inserted')
-        }
-    })
-
-})
-
+});
 
 module.exports = router;
